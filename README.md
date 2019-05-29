82204a0b31783d322eb8a703d2ad2a73bb2cb84a
Clyde D'Souza <clyd94@gmail.com>
2019-05-29 20:52:40 +1200

    Initial scaffolding for package

---
+++ README.md
@@ -0,0 +1,17 @@
+# Robohash Avatars
+TODO
+
+## Usage  
+TODO
+   
+## Examples 
+TODO
+
+## Release notes 
+Release notes can be found [here](https://github.com/ClydeDz/robohash-avatars-npm/releases).   
+
+## License
+This npm package is licensed under MIT.  
+
+## Credits  
+This npm package is developed by [Clyde D'Souza](https://clydedsouza.net).
---
+++ tests/robohash-avatars.test.js
@@ -0,0 +1,13 @@
+const uiavatars = require("../lib/robohash-avatars");
+const assert = require("assert"); 
+
+describe("generateAvatar()", function () {
+    console.log("[test suite]", this.title);
+
+    it("no input supplied", function () {
+        console.log("\t", "[test]", this.test.title);
+        var actual = true; 
+        assert.equal(actual, true);
+    }); 
+    
+});
