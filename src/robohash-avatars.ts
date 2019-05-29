d3af3f9f94a52c0114f8e666f1fcabe59bc83463
Clyde D'Souza <clyd94@gmail.com>
2019-05-30 07:54:33 +1200

    Create generate avatar method and add custom types

--- src/robohash-avatars.ts
+++ src/robohash-avatars.ts
@@ -1,11 +1,47 @@
+const baseURL = "https://robohash.org/";
 
-/**
- * Some comments
- */
-function abc(): number{
-    return 2+3
+enum CharacterSets{
+    Robots = "set_set1",
+    Monsters = "set_set2",
+    DisembodiedHeads = "set_set3",
+    Kittens = "set_set4"
 }
 
-abc();  
+enum BackgroundSets{
+    RandomBackground1 = "bgset_bg1",
+    RandomBackground2 = "bgset_bg2"
+}
 
-console.log("sssss");
+/**
+ * Checks if supplied setting exists and valid and constructs the query string part
+ * of the API URL accordingly.
+ * @param key The key part of the query string.
+ * @param setting The setting that needs to be supplied as the value for that key.
+ */
+function resolveSettings(key:string, setting: any) : string{
+    if(!setting){
+        return "";
+    }
+    return  `${key}=${setting}&`;
+}
+
+/**
+ * 
+ * @param settings 
+ * @example https://robohash.org/set_set4/bgset_bg1/3.14159?size=500x500
+ */
+export function generateAvatar(username: string, characters?: CharacterSets, 
+    background?: BackgroundSets, height?: number, width?: number): string {
+    let apiURL = baseURL;
+
+    if(!username){
+        username = "random hash"; //TODO: generate random hash
+    }
+    
+    apiURL += `${username}?`;
+    apiURL += resolveSettings("set", characters);
+    apiURL += resolveSettings("bgset", background);
+    apiURL += resolveSettings("size", `${width}x${height}`);
+
+    return apiURL;
+}
