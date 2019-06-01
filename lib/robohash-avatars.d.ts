a93ee6f7e448eb6d11080757101e593ce28939e6
Clyde D'Souza <clyd94@gmail.com>
2019-06-01 23:25:02 +1200

    Update comments for the API method

--- lib/robohash-avatars.d.ts
+++ lib/robohash-avatars.d.ts
@@ -1,8 +1,8 @@
 import { RobohashAvatarSettings } from "./models";
 export { CharacterSets, BackgroundSets, RobohashAvatarSettings } from "./models";
 /**
- *
- * @param settings
- * @example https://robohash.org/set_set4/bgset_bg1/3.14159?size=500x500
+ * Generates a RoboHast Avatar image URL that you can supply to your `<img/>` tag.
+ * @param settings {RobohashAvatarSettings} A settings object with any custom values.
+ * @example Returns a URL for your image like: https://robohash.org/RobohashAvatarNPM?bgset=bg1&size=200x200
  */
 export declare function generateAvatar(settings: RobohashAvatarSettings): string;
